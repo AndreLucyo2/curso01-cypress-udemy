0c85ab8fd7f6ebf838bbb6233b1ac315d5be5533
Andre Lucio <andre.lucio@insidesistemas.com.br>
2022-04-25 12:34:46 -0300

    arrow function 1

--- cypress/integration/arrow.spec.js
+++ cypress/integration/arrow.spec.js
@@ -1,7 +1,13 @@
 it('nada agora', function () { })
 
-const soma = function (a, b) {
+// const soma = function (a, b) {
+//     return a + b
+// }
+
+const soma = (a, b) => {
     return a + b
 }
+
+
 
 console.log(soma(10, 10))
