022331cc39a7b15941cb280f72bec463e8e9eb88
Andre Lucio <andre.lucio@insidesistemas.com.br>
2022-04-25 13:22:03 -0300

    grupo de testes

--- cypress/integration/describe.spec.js
+++ cypress/integration/describe.spec.js
@@ -5,6 +5,19 @@ it('A external test....', () => {
 });
 
 describe('Should group tests', () => {
+
+    describe('Should group more especific tests', () => {
+        it('A especific test...', () => {
+
+        });
+    });
+
+    describe('Should group more especific tests', () => {
+        it('A especific test...', () => {
+
+        });
+    });
+
     it('A internal test...', () => {
 
     });
