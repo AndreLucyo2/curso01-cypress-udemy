45de9f7a897156601f620aea14fe96adb1fc6ec5
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-10 16:42:37 -0300

    teste cam campo de texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -13,11 +13,11 @@ describe('Work with basic elements', () => {
         cy.reload()
     });
 
-    it('Text', ()=>{
-        
+    it('Text', () => {
+
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
-       
+
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     });
 
@@ -31,8 +31,36 @@ describe('Work with basic elements', () => {
         //Validar que o elemento não texto nao contem
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
-        cy.get('#resultado').should('have.text', 'Voltou!')        
-        
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
     });
-    
+
+    it.only('TextFields', () => {
+
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        //obtem o elemento: manda digitar o texto e valida se esta com o texto
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        //Selecionar emento da tabela:
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+
+        //Executa teclas junto com o texto:
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        //obtem o elemento: manda digitar o texto e valida se esta com o texto
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{Selectall}acerto', {delay:500})
+            .should('have.value', 'acerto')
+
+
+    });
+
 });
