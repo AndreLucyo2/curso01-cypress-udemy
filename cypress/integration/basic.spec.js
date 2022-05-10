85fb25a73e48db5b28b0e822b4f7a1c9aadd0057
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-10 13:55:44 -0300

    Criado assert para teste em botão

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -2,8 +2,37 @@
 
 //https://www.wcaquino.me/cypress/componentes.html
 
-describe('Cyoress basics', () => {
+describe('Cypress basics', () => {
     it('Should visit a page and assert title', () => {
-        
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        //Deve ser de forma assincrona
+        // const title = cy.title()
+        // console.log(title)
+
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .should('contain', 'Campo')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+
+        //Desafios :
+        //TODO imprimir o titulo no console
+        //TODO escrever o log em um campo de texto na pagina
+    });
+
+    it.only('Should find and with an element', () => {
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+
     });
 });
