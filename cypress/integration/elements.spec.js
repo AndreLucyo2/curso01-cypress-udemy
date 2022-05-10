eb57337288856e28eba2896c03e7949886e4cc89
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-10 14:26:26 -0300

    fazendo reload com beforeach

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -2,11 +2,12 @@
 
 describe('Work with basic elements', () => {
 
-    //Executa antes de cada test
+    //Executa uma vez antes de cada test
     before(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
     });
 
+    //Executa antes de cada teste
     beforeEach(() => {
         //Recarregar a pagina
         cy.reload()
