fc7f96a54bf0f976843722a09ad82341ec03e2a0
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-10 14:20:14 -0300

    usando reload e contias para buscar elements

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -8,6 +8,22 @@ describe('Work with basic elements', () => {
         cy.get('span').should('contain', 'Cuidado')
        
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
-    })
+    });
+
+    it.only('Links', () => {
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        //Recarregar a pagina
+        cy.reload()
+        //Validar que o elemento não texto nao contem
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')        
+        
+    });
     
 });
