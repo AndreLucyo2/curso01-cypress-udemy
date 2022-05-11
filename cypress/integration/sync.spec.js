e927b2c083701fb33f91b452a0366646a9afb489
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-11 08:59:28 -0300

    teste de espera sync

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,28 @@
+///<reference types = "cypress"/>
+
+describe('Esperas...', () => {
+
+    //Executa uma vez antes de cada test
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    });
+
+    //Executa antes de cada teste
+    beforeEach(() => {
+        //Recarregar a pagina
+        cy.reload()
+    });
+
+    it('Deve aguardar elemento estar disponivel', () => {
+        
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('funcionou')
+        
+    });
+
+
+    
+});
