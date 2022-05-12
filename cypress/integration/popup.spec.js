6bbfc82fd7b4c0b7f97f198a35dffb23320b449b
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-12 17:36:34 -0300

    trabalhando com popup

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -9,20 +9,20 @@ describe('Work with popup', () => {
 
         cy.get('#otherButton').click()
 
-        cy.on('window:alert', msg =>{
+        cy.on('window:alert', msg => {
             expect(msg).to.be.equal('Click OK!')
         })
-        
+
     });
 
-    it.only('Deve verificar se o popup foi envocado', () => {
+    it('Deve verificar se o popup foi envocado', () => {
 
         cy.visit('https://wcaquino.me/cypress/componentes.html')
-        
+
         //Mocar o evento da window
-        cy.window().then(win=>{
+        cy.window().then(win => {
             //Criar o evento e dar um alias
-            cy.stub(win, 'open').as('winOpen') 
+            cy.stub(win, 'open').as('winOpen')
         })
 
         cy.get('#buttonPopUp').click()
@@ -30,6 +30,57 @@ describe('Work with popup', () => {
         //Obtem o evendo alias, e faz o acerto para ver se o evento foi chamado
         cy.get('@winOpen').should('be.called')
 
+    });
+
+    //Teste com pagina sendo carregada em outra aba
+    describe.only('With links', () => {
+
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+
+        //Validar se esta apontando para a url correta
+        it('Check popup url', () => {
+
+            //Validar se possui a url:
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+
+        });
+
+        //Acessar a popup dinamicamente, valida a pagina
+        it('Should access popup dinamically', () => {
+
+            //Obtem uma url dinamicamente
+            //Este metodo é util quando o link não é fixo, e nao se sabe o link no momento
+            //Encontra o elemento que redireciona para a outra pagina e obtem o link
+            cy.contains('Popup2').then($a => {
+
+                //Obtem a propriedade href que possui o link
+                const href = $a.prop('href')
+
+                //printa o link para conferir
+                cy.log(href)
+
+                //manda visitar a pagina
+                cy.visit(href)
+                cy.get('#tfield').type('Funcionou!')
+            })
+        });
+
+        //força abrir popup na mesma pagina, remove o atributo target
+        //facilita o teste com cypress
+        it('Should force link on same page', () => {
+
+            //Remove o atributo target forçando a abrir na mesma pagina
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+
+            //manda o texto
+            cy.get('#tfield').type('Funcionou!')
+        });
 
     });
 
