c931169b0562a578013eb66e749e5cf0bac8d300
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-13 15:03:23 -0300

    Get data form fixture file json

---
+++ cypress/integration/fixture.spec.js
@@ -0,0 +1,30 @@
+///<reference types = "cypress"/>
+
+describe('Fixtures testes', () => {
+
+    it('Get data form fixture file', function() {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        //Obtem os dados mokados: busca direto na pasta de fixtures
+        cy.fixture('userData').as('usuario').then(() => {
+
+            cy.get('#formNome').type(this.usuario.nome)
+            cy.get('#formSobrenome').type(this.usuario.sobrenome)
+            cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click()
+            cy.get(`[name=formComidaFavorita][value=${this.usuario.comida}]`).click()
+            
+            cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+            cy.get('#formEsportes').select(this.usuario.esportes)
+
+            //Clicar em cadastrar
+            cy.get('#formCadastrar').click()
+    
+            //Valida se apareceu a informação de cadastrado
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado')
+
+        })   
+
+
+    })
+
+})
