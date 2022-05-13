53ab27acb7ecf28214fb30d7728bde24e997d254
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-05-13 16:03:00 -0300

    executando testes -- Dinamic tests

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -1,39 +1,45 @@
 /// <reference types="cypress" />
 describe('Dinamic tests', () => {
 
-    before(() => {
+    beforeEach(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
     })
 
-    const food = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
+    //Tipos de comida:
+    const foods = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
 
-    it('Cadastro com comida variada', function() {
-        
-               //Obtem os dados mokados: busca direto na pasta de fixtures
-               cy.fixture('userData').as('usuario').then(() => {
+    //faz um laço com os elementos do array
+    foods.forEach(food =>{
 
-                cy.get('#formNome').type(this.usuario.nome)
-                cy.get('#formSobrenome').type(this.usuario.sobrenome)
-                cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click()
-
-                //cy.get(`[name=formComidaFavorita][value=${this.usuario.comida}]`).click()
-                
-                //locator com xpatch -- instalar plugin : seção 7 Xpath
-                // https://docs.cypress.io/plugins/directory
-                // https://github.com/cypress-io/cypress-xpath                
-                cy.xpath(`//label[contains(., 'Carne')]/preceding-sibling::input`).click()
-
-                cy.get('#formEscolaridade').select(this.usuario.escolaridade)
-                cy.get('#formEsportes').select(this.usuario.esportes)
+        //Vai executar um teste para cada elemento do array:
+        it(`Cadastro com comida variada: '${food}'`, function() {
+            
+                   //Obtem os dados mokados: busca direto na pasta de fixtures
+                   cy.fixture('userData').as('usuario').then(() => {
     
-                //Clicar em cadastrar
-                cy.get('#formCadastrar').click()
-        
-                //Valida se apareceu a informação de cadastrado
-                cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado')
-    
-            })   
+                    cy.get('#formNome').type(this.usuario.nome)
+                    cy.get('#formSobrenome').type(this.usuario.sobrenome)
+                    cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click()
 
-    });
+                    //locator com xpatch -- instalar plugin : seção 7 Xpath
+                    // https://docs.cypress.io/plugins/directory
+                    // https://github.com/cypress-io/cypress-xpath                
+                    cy.xpath(`//label[contains(.,'${food}')]/preceding-sibling::input`).click()
+    
+                    cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+                    cy.get('#formEsportes').select(this.usuario.esportes)
+        
+                    //Clicar em cadastrar
+                    cy.get('#formCadastrar').click()
+            
+                    //Valida se apareceu a informação de cadastrado
+                    cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado')
+        
+                })  
+    
+        });
+
+    })
+
 
 })
