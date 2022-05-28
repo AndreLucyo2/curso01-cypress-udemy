7721ac8b39cbd4e190351c0d6bad0b732da988e6
AndreLucyo <andre.lucio@nextar.com.br>
2022-05-28 17:05:38 -0300

    Create test case Create an account

--- cypress/integration/funcional.spec.js
+++ cypress/integration/funcional.spec.js
@@ -5,9 +5,10 @@ describe('Sholud test at a functional level', () => {
 
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/') 
+        cy.login('andre.lucyo2@gmail.com','123456');
     });
 
-    it('Login', () => {
+    xit('Login', () => {
         //Arrange
         cy.get('.input-group > .form-control').type('andre.lucyo2@gmail.com')
         cy.get('[data-test="passwd"]').type('123456');
@@ -20,17 +21,18 @@ describe('Sholud test at a functional level', () => {
 
     });
 
-    it.only('...', () => {
-
+    it.only('Create an account', () => {
         //Arrange
-        cy.login('andre.lucyo2@gmail.com','123456');
-
-        //Act
-
-
-        //Assert:
-
+        cy.get('[data-test="menu-settings"]').click();
+        cy.get('[href="/contas"]').click();
+        cy.get('[data-test="nome"]').should('be.visible').type('Conta Teste - '+ Date.now())
         
+        //Act
+        cy.get('.btn').should('be.visible').click();
+                
+        //Assert
+        cy.get('.toast-message').should('contain','Conta inserida com sucesso');
+
     });
     
 });
