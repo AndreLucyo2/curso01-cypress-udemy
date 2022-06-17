2bc015f721d5f9c4d224737dbf9abdc0459f2bcd
AndreLucyo <andre.lucio@nextar.com.br>
2022-06-17 10:18:05 -0300

    criado teste da api para cria um conta e valida se deu sucesso

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -3,6 +3,8 @@
 
 describe('Sholud test at a functional level', () => {
 
+    const fakeNomeConta = 'Conta criada - rest_089'
+
     it('Create an account', () => {
         cy.request({
             method: 'POST',
@@ -13,20 +15,28 @@ describe('Sholud test at a functional level', () => {
                 redirecionar: false
             }
         }).its('body.token').should('not.be.empty') //.then(resp => console.log(resp));
-        //Obtem o tokem a partir do login: Faz o POST de uma nova conta
-        .then(token =>{
-            cy.request({
-                url:'https://barrigarest.wcaquino.me/contas',
-                method:'POST',
-                headers:{Authorization:`JWT ${token}`},
-                body:{
-                    nome:'Conta criada - rest_003'
-                }
-                
-            }).then(resp=>{ console.log(resp)});
+            //Obtem o tokem a partir do login: Faz o POST de uma nova conta
+            .then(token => {
+                cy.request({
+                    url: 'https://barrigarest.wcaquino.me/contas',
+                    method: 'POST',
+                    headers: { Authorization: `JWT ${token}` },
+                    body: {
+                        nome: fakeNomeConta
+                    }
+                }).as('response');//Adicionado um nome para o retorno para usar mais para frente nos testes
+            })
 
-            //Print do token: estrategia de token usado é o JWT- este é um método mais antigo
-            cy.log(token);
+        cy.get('@response').then(resp => {
+            //cy.log(resp);
+
+            //Testa se retornou sucesso:
+            expect(resp.status).to.be.equals(201);
+            //Testa se no body contem a propriedade chamada id
+            expect(resp.body).to.have.property('id');
+            //Testa se no body contem a propriedade chamada nome com o nome da conta criada
+            expect(resp.body).to.have.property('nome', fakeNomeConta);
+
 
         })
 
