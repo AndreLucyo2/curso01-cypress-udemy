989b4c172570b44927f1941766d38fc683e6de95
AndreLucyo <andre.lucio@nextar.com.br>
2022-06-17 10:25:34 -0300

    refatorado teste para receber o token do commando personalizado

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -3,29 +3,20 @@
 
 describe('Sholud test at a functional level', () => {
 
-    const fakeNomeConta = 'Conta criada - rest_089'
+    const fakeNomeConta = 'Conta criada - rest_011'
 
     it('Create an account', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://barrigarest.wcaquino.me/signin',
-            body: {
-                email: "andre.lucyo2@gmail.com",
-                senha: "123456",
-                redirecionar: false
-            }
-        }).its('body.token').should('not.be.empty') //.then(resp => console.log(resp));
-            //Obtem o tokem a partir do login: Faz o POST de uma nova conta
-            .then(token => {
-                cy.request({
-                    url: 'https://barrigarest.wcaquino.me/contas',
-                    method: 'POST',
-                    headers: { Authorization: `JWT ${token}` },
-                    body: {
-                        nome: fakeNomeConta
-                    }
-                }).as('response');//Adicionado um nome para o retorno para usar mais para frente nos testes
-            })
+
+        cy.getToken("andre.lucyo2@gmail.com", "123456").then(token => {
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/contas',
+                method: 'POST',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    nome: fakeNomeConta
+                }
+            }).as('response');//Adicionado um nome para o retorno para usar mais para frente nos testes
+        })
 
         cy.get('@response').then(resp => {
             //cy.log(resp);
